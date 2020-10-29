171e1901d137254ab85639298de1b9489b800a47
Suminkim123 <mini_1007@naver.com>
2020-10-29 16:49:32 +0900

    04

--- main2.c
+++ main2.c
@@ -3,24 +3,40 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
+
+void print_array(int a[], int size)
+
+
 int main(int argc, char *argv[]) {
 	
 	//array declaration
-	int i, 
-	int grade[SIZE];
-	int score[SIZE];
+	int list[SIZE] = {1, 2, 3, 4};
 	
-	for (i=0;<SIZE;i++)
-	{
-		grade[i] = rand()%100+1;
-	}
+    printf_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
+    
+    return 0;
+    
+}
+
+void square_array(int a[], int size)
+{
+	int i;
+	for (i=0;i<size;i++)
+	    a[i] = a[i]*a[i];
+}
+
+void print_array(int a[], int size)	
+{
+	int i;
+	for (i=0;i<size;i++)
+	    printf("%3d ", a[i]);
+	printf("/n");	
 	
-	//move to score
-	for (i=0;i<SIZE;1++)
-	    score[i] = grade[i];
-	    
-	for (i=0;i<SIZE;i++)    
-	    printf("score[%d] %i (%i)/n", i, score[i], grade[i]);
+	
+	
 	
 	     
 	return 0;
