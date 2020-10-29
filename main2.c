f0899e809f349826fc1db0e84c75bfceb7c1e8d8
Suminkim123 <mini_1007@naver.com>
2020-10-29 16:08:41 +0900

    02

---
+++ main2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	//array declaration
+	int i, average;
+	int sum
+	int grade[SIZE];
+	
+	printf("input %i scores/n", SIZE);
+	
+	for (i=0;i<SIZE;1++)
+	    scanf("%d", &grade[1]);
+	   
+	for (i=0;i<SIZE;i++) 
+	     printf("grade[%d] = %d/n", i, grade[1]);   
+	return 0;
+}
